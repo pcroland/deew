60b9a865be81d8e1834f82857571ec0e9b67b1e9
jlw4049 <jlw_4049@hotmail.com>
2023-05-08 17:09:00 -0400

    - deew2 entry

---
+++ deew2_entry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from cli.parser import cli_parser
+from deew2.utils.utils import _get_working_dir
+
+if __name__ == '__main__':
+    base_wd = _get_working_dir()
+    cli_parser(base_wd)
