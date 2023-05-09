a139d1a1a34f26b55cc13c6c8d14533310c136b6
jlw4049 <jlw_4049@hotmail.com>
2023-05-08 23:03:59 -0400

    - Added a base payload, this will need more work over all, but is functional

---
+++ deew/payloads/__init__.py
@@ -0,0 +1,31 @@
+class DeePayload:
+    # TODO This needs to be split up, but can't be until everything else is
+    file_input = None
+    track_index = None
+    # file_output = None
+    output_dir = None
+    encoder_format = None
+    bitrate = None
+    downmix = None
+    delay = None
+    drc = None
+    dialnorm = None
+    instances = None
+    measure_only = None
+    force_standard = None
+    force_bluray = None
+
+    list_bitrates = None
+    long_argument = None
+    no_prompt = None
+    print_logo = None
+    changelog = None
+    config = None
+    generate_config = None
+
+    # temp_dir = None
+    # keep_temp = None
+    # progress_mode = None
+    # stereo_mix = None
+    # ffmpeg_path = None
+    # dee_path = None
